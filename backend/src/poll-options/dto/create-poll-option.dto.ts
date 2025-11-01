34f3f65cb7c2b3431b706bcc2331fe6253126ba6
Samrat <ksamrat224@gmail.com>
2025-11-01 16:38:16 +0545

    dto implemented for polloptions

--- backend/src/poll-options/dto/create-poll-option.dto.ts
+++ backend/src/poll-options/dto/create-poll-option.dto.ts
@@ -1,3 +1,11 @@
+import { IsInt, IsNotEmpty, IsString } from "class-validator";
+
 export class CreatePollOptionDto {
+    @IsNotEmpty()
+    @IsString()
+    text:string;
     
+    @IsNotEmpty()
+    @IsInt()
+    pollId:number;
 }
