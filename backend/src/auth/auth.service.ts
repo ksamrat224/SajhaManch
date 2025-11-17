e81518155712ce83261235a5b463b58ae275b805
Samrat <ksamrat224@gmail.com>
2025-11-17 22:08:44 +0545

    feat: add auth module with controller, service, and DTO

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaClient,
+    private readonly usersService: UsersService,
+  ) {}
+  async register(registerDto: RegisterDto) {
+    const user = await this.usersService.create(registerDto);
+    return user;
+  }
+}
