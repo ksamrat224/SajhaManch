a8bd2ea8fb3cf854186cbf51c27b4669a39045f6
Samrat <ksamrat224@gmail.com>
2025-11-20 16:18:39 +0545

    refactor: reorganize auth guard ,middleware updated with guard

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 async function bootstrap() {
   //application instance is created here
@@ -14,6 +16,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  //Middleware for guard and authentication
+  app.useGlobalGuards(new AuthGuard(new JwtService()));
 
   //application starts here
   await app.listen(process.env.PORT ?? 3000);
