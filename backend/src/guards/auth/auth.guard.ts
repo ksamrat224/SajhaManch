d2bf5934b8d8bd6c1a1d289387e2222e066bbf96
Samrat <ksamrat224@gmail.com>
2025-11-20 17:49:15 +0545

    feat: updated backend auth ,added helper and reflector in middleware also used public in auth controller

--- backend/src/guards/auth/auth.guard.ts
+++ backend/src/guards/auth/auth.guard.ts
@@ -1,11 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { PUBLIC_KEY } from 'src/helpers/public';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly reflector: Reflector,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
+      const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [
+        context.getHandler(),
+        context.getClass(),
+      ]);
+      if (isPublic) {
+        return true;
+      }
       const request = context.switchToHttp().getRequest(); //request object
       const [type, token] = request.headers.authorization?.split(' ') ?? [];
       if (type !== 'Bearer' || !token) {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './guards/auth/auth.guard';
@@ -18,7 +18,7 @@ async function bootstrap() {
   );
 
   //Middleware for guard and authentication
-  app.useGlobalGuards(new AuthGuard(new JwtService()));
+  app.useGlobalGuards(new AuthGuard(new JwtService(),new Reflector()));
 
   //application starts here
   await app.listen(process.env.PORT ?? 3000);
