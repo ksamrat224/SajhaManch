158e501b57b6d8e6c6b037103b37120d0b407dd2
Samrat <ksamrat224@gmail.com>
2026-01-23 14:36:11 +0545

    refactor: add PaginatedResponse interface and import PollsModule in votes module

--- backend/src/votes/votes.module.ts
+++ backend/src/votes/votes.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { VotesService } from './votes.service';
 import { VotesController } from './votes.controller';
 import { PrismaService } from '../../prisma/prisma.service';
+import { PollsModule } from '../polls/polls.module';
 
 @Module({
+  imports: [PollsModule],
   controllers: [VotesController],
   providers: [VotesService, PrismaService],
 })
